1f7a053e14ae1bae99a7a1742a1a381e5eefbead
johncross00 <johncross.ktm@gmail.com>
2024-01-29 13:24:33 +0000

    .

---
+++ lib/exemple2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Exemple2 extends StatelessWidget {
+  const Exemple2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
