8873b87e226790d4c893d0aed9cb699d35c4551e
johncross00 <johncross.ktm@gmail.com>
2024-01-29 14:28:53 +0000

    ....

--- lib/exemple2.dart
+++ lib/exemple2.dart
@@ -6,13 +6,19 @@ class Exemple2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        children: [
-          ShowBanner(),
-          ShowTitleOne(),
-          ShowTitleTwo(),
-          ShowImageAndText(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ShowBanner(),
+            ShowTitleOne(),
+            ShowTitleTwo(),
+            ShowImageAndText(),
+            ShowText(),
+            ShowIconAndLabel(),
+            ShowTwoImages(),
+            ShowButton()
+          ],
+        ),
       ),
     );
   }
@@ -45,7 +51,7 @@ class ShowTitleTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text("SAMSUNG", style: TextStyle(
-      color: Colors.blue,
+      color: Colors.purple,
       fontWeight: FontWeight.w600,
       fontSize: 20
     ),);
@@ -59,11 +65,78 @@ class ShowImageAndText extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-      Image.asset("assets/images/apple.jpg",)
+      Image.asset("assets/images/vision.png",
+      width: 200,
+      height: 200,),
+        Text('Apple Vision PRO',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w700,
+          fontSize: 20,
+        ),)
       ],
     );
   }
 }
+class ShowText extends StatelessWidget {
+  const ShowText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ShowIconAndLabel extends StatelessWidget {
+  const ShowIconAndLabel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ShowTwoImages extends StatelessWidget {
+  const ShowTwoImages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Image.asset(
+          "assets/images/flip.jpg",
+          width: 150,
+          height: 150,
+        ),
+        Image.asset(
+          "assets/images/flip.jpg",
+          width: 150,
+          height: 150,
+        )
+      ],
+    );
+  }
+}
+
+class ShowButton extends StatelessWidget {
+  const ShowButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: (){},
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(Colors.purple),
+      ),
+        child: Text("Voir Plus", style: TextStyle(color: Colors.white),),
+    );
+  }
+}
+
+
+
+
 
 
 
