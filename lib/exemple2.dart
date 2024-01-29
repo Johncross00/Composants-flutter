b7f8db4735ee8785525d07129885900f88e4ebc1
johncross00 <johncross.ktm@gmail.com>
2024-01-29 13:53:42 +0000

    ...

--- lib/exemple2.dart
+++ lib/exemple2.dart
@@ -5,6 +5,65 @@ class Exemple2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Column(
+        children: [
+          ShowBanner(),
+          ShowTitleOne(),
+          ShowTitleTwo(),
+          ShowImageAndText(),
+        ],
+      ),
+    );
   }
 }
+class ShowBanner extends StatelessWidget {
+  const ShowBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/images/flip.jpg", width: 200, height: 350,);
+  }
+}
+
+class ShowTitleOne extends StatelessWidget {
+  const ShowTitleOne({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Galaxy Z Flip 4", style: TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w700,
+      fontSize: 40
+    ),);
+  }
+}
+
+class ShowTitleTwo extends StatelessWidget {
+  const ShowTitleTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("SAMSUNG", style: TextStyle(
+      color: Colors.blue,
+      fontWeight: FontWeight.w600,
+      fontSize: 20
+    ),);
+  }
+}
+
+class ShowImageAndText extends StatelessWidget {
+  const ShowImageAndText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+      Image.asset("assets/images/apple.jpg",)
+      ],
+    );
+  }
+}
+
+
+
